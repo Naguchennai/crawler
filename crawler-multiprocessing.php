4ed15aae74b3b9921286fbdc65d0da050358c424
arosa <ariel.rosa@skygate.com.pl>
2013-03-27 08:48:47 +0100

    Add create file sitemap index

--- crawler-multiprocessing.php
+++ crawler-multiprocessing.php
@@ -18,7 +18,7 @@ class MyCrawler extends PHPCrawler
       $this->sitemap_output_file = $file;
 
       file_put_contents($this->sitemap_output_file,
-                      '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n',
+                      "<?xml version=\"1.0\" encoding=\"UTF-8\"?><urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">\r\n",
                       FILE_APPEND);
   }
 
@@ -58,7 +58,21 @@ class MyCrawler extends PHPCrawler
 
   public function closeFile()
   {
-    file_put_contents($this->sitemap_output_file, '</urlset>', FILE_APPEND);
+        file_put_contents($this->sitemap_output_file, '</urlset>', FILE_APPEND);
+    
+        // Create file sitemap index
+        $content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n".
+                        "<sitemapindex xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\r\n".
+                           "<sitemap>\r\n".
+                              "<loc>http://www.primerates.com/sitemap_primerates.xml</loc>\r\n".
+                        "<lastmod>".date('Y-m-d', strtotime('-1 day'))."</lastmod>\r\n".
+                           "</sitemap>\r\n".
+                           "<sitemap>\r\n".
+                              "<loc>http://www.primerates.com/listRatesInSiteMap.xml</loc>\r\n".
+                              "<lastmod>".date('Y-m-d', strtotime('-1 day'))."</lastmod>\r\n".
+                           "</sitemap>\r\n".
+                        "</sitemapindex>\r\n";
+        file_put_contents('sitemap.xml', $content, FILE_APPEND);
   }
 }
 
@@ -67,7 +81,8 @@ class MyCrawler extends PHPCrawler
 // and start the crawling-process. 
 
 $crawler = new MyCrawler();
-$crawler->setSitemapOutputFile("sitemap.xml");
+// File name sitemap page primerates but not rates 
+$crawler->setSitemapOutputFile("sitemap_primerates.xml");
 // URL to crawl
 $crawler->setURL("primerates.com");
 
